ae7aed8e5bf4ba99abb1a8c96cb8a750b3e1bb5e
Paul Yi <pauljsyi@gmail.com>
2025-01-15 23:43:09 -0700

    selection sort

--- week_one/brute_force.js
+++ week_one/brute_force.js
@@ -1,1 +1,17 @@
-function selectionSort(A) {}
+function selectionSort(a) {
+  // red value searches the array for a lower value
+  for (let i = 0; i < a.length; i++) {
+    let minvalue = a[i];
+    let minindex = i;
+    for (let red = i + 1; red < a.length; red++) {
+      if (a[red] < minvalue) {
+        minvalue = a[red];
+        minindex = red;
+      }
+    }
+    [a[i], a[minindex]] = [a[minindex], a[i]];
+  }
+  return a;
+}
+
+console.log(selectionSort([3, 2, 1, 16, 8, 2, 4, 3]));
